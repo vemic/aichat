a2ae81855443e8d9dbe1b811c6b7ad8f122a6ae4
Hiroki Kurosawa <s@vemi.jp>
2025-05-18 21:14:57 +0900

    Appコンポーネントの構造を変更し、レイアウトとテーマ、ポイント、チャットのプロバイダーを統合

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from './components/layout/Layout';
+import ThemeProvider from './context/ThemeContext';
+import { PointsProvider } from './context/PointsContext';
+import { ChatProvider } from './context/ChatContext';
+import { CssBaseline } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <CssBaseline />
+      <PointsProvider>
+        <ChatProvider>
+          <Layout />
+        </ChatProvider>
+      </PointsProvider>
+    </ThemeProvider>
   );
 }
 
